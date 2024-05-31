cd8cc03af3df1b36ed6271d8f2aa223682f40ad7
Mahin Hasan Upol <83445053+mahinupol@users.noreply.github.com>
2024-05-31 21:18:18 +0600

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Hero from './Components/Share/Hero';
+import Navigation from './Components/Share/Navigation';
+
+function App() {
+  return (
+    <div className="App">
+      <Navigation/>
+      <Hero/>
+    </div>
+  );
+}
+
+export default App;
